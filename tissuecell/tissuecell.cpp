9a49efad6123be7a9a54d77dd4d7b153c37993d8
John Haberstroh <jhaberstroh@berkeley.edu>
2013-06-26 18:08:55 -0700

    Slight refactor into .h file, created testing folder

--- tissuecell/tissuecell.cpp
+++ tissuecell/tissuecell.cpp
@@ -1,25 +1,7 @@
-#include <iostream>
-#include <vector>
-
-// Create the data struct with either a float or a double to vary precision
-template <class Precision>
-	struct Data
-{
-	typedef Precision value_type;
-	value_type angle;
-	value_type x;
-	value_type y;
-	Data(value_type mx, value_type my, value_type ma)
-	{
-		x = mx;
-		y = my;
-		angle = ma;
-	}
-};
-
+#include "tissuecell.h"
 
 int main(){
-	typedef Data<float> mydat;
-	std::vector<mydat> myvector(10,mydat(1.3, 1.3, 10));
-	std::cout << myvector[5].x << std::endl;
+	TissueCell::Unit x = TissueCell::Unit::Create_xya(1.3, 1.3, 3.14);
+	TissueCell::Vector myvector(10, x);
+	std::cout << typeid(myvector[5].x).name() << std::endl;
 }
---
+++ tissuecell/tissuecell.h
@@ -0,0 +1,43 @@
+// tissuecell.h
+//================================================
+// Defines the TissueCell namespace.
+// In this namespace, we define typedefs for a single cell (Unit) and a vector of cells (Collection).
+
+
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+
+#ifdef DP
+#define MACRO_CELLTYPE CellData<double>
+#else
+#define MACRO_CELLTYPE CellData<float>
+#endif
+
+namespace TissueCell{
+
+	// Create the data struct with either a float or a double to vary precision
+	template <class sp_or_dp>
+		struct CellData
+	{
+		typedef sp_or_dp value_type;
+		value_type angle;
+		value_type x;
+		value_type y;
+	
+		CellData(value_type mx, value_type my, value_type ma): x(mx), y(my), angle(ma) {};
+		CellData(): x(0), y(0), angle(0) {};
+
+		static CellData<value_type> Create_xya(value_type mx, value_type my, value_type ma)
+		{
+			return CellData(mx,my,ma);
+		}
+	};
+
+	typedef MACRO_CELLTYPE Unit;
+	typedef std::vector<Unit> Vector;
+
+		
+
+
+}
